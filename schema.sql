749fc5745e20c7dde9f00e5adf15915e6c505ded
Jenni Nylund <jenni.esprit@gmail.com>
2021-09-26 20:21:11 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,13 @@
 CREATE TABLE restaurants 
         (id SERIAL PRIMARY KEY, name TEXT, phone TEXT, email TEXT, description TEXT);
-CREATE TABLE 
-        styles (id SERIAL PRIMARY KEY, restaurants_id REFERENCES style TEXT);
-CREATE TABLE 
-        users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BOOLEAN);
-CREATE TABLE 
-        reviews (id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, user_id INTEGER REFERE>
-CREATE TABLE 
-        menu (id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, menu TEXT, price FLOAT);
+CREATE TABLE styles 
+        (id SERIAL PRIMARY KEY, restaurants_id REFERENCES style TEXT);
+CREATE TABLE users 
+        (id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BOOLEAN);
+CREATE TABLE reviews 
+        (id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, user_id INTEGER REFERENCES users, comment TEXT, stars INTEGER);
+CREATE TABLE menu 
+        (id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, menu TEXT, price FLOAT);
+
+
+
