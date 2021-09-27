d439bb542bef6150dde9fa5a99fa283b6d633674
Jenni Nylund <jenni.esprit@gmail.com>
2021-09-27 13:44:50 +0300

    schema update

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE restaurants 
+	(id SERIAL PRIMARY KEY, name TEXT, phone TEXT, email TEXT, description TEXT);
+CREATE TABLE styles 
+	(id SERIAL PRIMARY KEY, restaurants_id REFERENCES style TEXT);
+CREATE TABLE users 
+	(id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BOOLEAN);
+CREATE TABLE reviews 
+	(id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, user_id INTEGER REFERE>
+CREATE TABLE menu 
+	(id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, menu TEXT, price FLOAT);
