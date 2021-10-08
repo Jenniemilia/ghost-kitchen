741bc82dfa49d792f01c0b1d0d1a6f5f59322d4b
Jenni Nylund <jenni.esprit@gmail.com>
2021-10-08 11:22:59 +0300

    improve navigation

--- users.py
+++ users.py
@@ -1,33 +1,47 @@
+#from os import name
+import os
 from db import db
-from flask import session
+from flask import session, abort, request
 from werkzeug.security import check_password_hash, generate_password_hash
 
-def login(username, password):
-    sql = "SELECT id, password FROM users WHERE username=:username"
-    result = db.session.execute(sql, {"username":username})
+def login(name, password):
+    sql = "SELECT password, id, role FROM users WHERE name=:name"
+    result = db.session.execute(sql, {"name":name})
     user = result.fetchone()
     if not user:
         return False
-    else:
-        if check_password_hash(user.password, password):
-            session["user_id"] = user.id
-            return True
-        else:
-            return False
-
+    if check_password_hash(user.password, password):
+        session["user_id"] = user.id
+        session["user_name"] = name
+        session["user_role"] = user.role
+        session["csrf_token"] = os.urandom(16).hex()
+        return True
+    return False
 
 def user_id():
     return session.get("user_id", 0)
 
 def logout():
-    del session["username"]
+    del session["user_name"], session["user_id"], session["user_role"], session["csrf_token"]
 
-def register(username, password):
+def register(name, password, role):
 	hash_value = generate_password_hash(password)
 	try:
-		sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
-		db.session.execute(sql, {"username":username, "password":hash_value})
+		sql = "INSERT INTO users (name, password, role) VALUES (:name, :password, :role)"
+		db.session.execute(sql, {"name":name, "password":hash_value, "role":role})
 		db.session.commit()
 	except:
 		return False
-	return login(username, password)
+    
+	return login(name, password)
+
+def check_csrf():
+    if session["csrf_token"] != request.form["csrf_token"]:
+        abort(403)
+
+#def require_role(role):
+#    if role > session.get("user_role", 0):
+#        abort(403)
+
+
+
